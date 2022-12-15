01c67da6f6b420f53ec3fabe4915306916bc5b16
Dhananjai <dhananjai.eee@gmail.com>
2022-12-15 12:47:08 +0530

    Exception and logger updated

--- CO2/logger.py
+++ CO2/logger.py
@@ -1,17 +1,20 @@
-import os
 import logging
+import os
 from datetime import datetime
 
-# Creating log directory
-log_file_directory = os.path.join(os.getcwd(), 'logs')
-os.makedirs(log_file_directory, exist_ok=True)
+#log file name
+log_file_name= f"{datetime.now().strftime('%m-%d-%Y__%H-%M-%S')}.log"
 
-# creating log file name
-log_file_name = f"{datetime.now().strftime('%m-%d-%Y__%H:%M:%S')}.log"
-log_file_path = os.path.join(log_file_directory, log_file_name)
+# Log directory
+log_file_dir = os.path.join(os.getcwd(),'logs')
 
-# Writing the info to log file
+#create folder if not available
+os.makedirs(log_file_dir, exist_ok = True)
+
+# log file path
+log_file_path = os.path.join(log_file_dir, log_file_name)
+
 logging.basicConfig(
-    filename=log_file_path,
+    filename= log_file_path,
     format = '[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s',
     level = logging.INFO)
