5fe54352891642a34e3297138265cbc4e4da15f6
Dhananjai <dhananjai.eee@gmail.com>
2022-12-19 19:28:33 +0530

    Data validation configuration defined

--- CO2/entity/config_entity.py
+++ CO2/entity/config_entity.py
@@ -38,7 +38,13 @@ class DataIngestionConfig:
             raise CO2_Exception(e, sys)
 
 
-class DataValidationConfig: ...
+class DataValidationConfig:
+    def __init__(self, training_pipeline_config: TrainingPipelineConfig):
+        self.data_validation_dir = os.path.join(training_pipeline_config.artifact_dir, "data_validation")
+        self.report_file_path = os.path.join(self.data_validation_dir, "report.yaml")
+        self.missing_threshold:float = .1
+        self.base_file_path = os.path.join('XCO2_kenya.csv')
+        
 
 
 class DataTransformationConfig: ...
